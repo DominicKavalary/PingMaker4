2e9f7a7b56ddf2ea359a78e475559023960caa69
Dominic Kavalary <128938538+DominicKavalary@users.noreply.github.com>
2024-11-16 18:43:36 -0600

    Update remtargets.php

--- remtargets.php
+++ remtargets.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <a href="request.html">requests</a>
+    <a href="targets.html">targets</a>
     <title>messing around</title>
 </head>
 <body>
