e38a9e0d0be48e980d92a2f055ba9e85487d55ad
Dominic Kavalary <128938538+DominicKavalary@users.noreply.github.com>
2024-11-30 11:22:16 -0600

    Update request.php

--- request.php
+++ request.php
@@ -15,6 +15,57 @@
     Name: <input type="text" name="target"><br>
     <input type="submit">
     </form>
+
+<button type="button" onclick="tableToCSV()">
+            download CSV
+</button>
+<script type="text/javascript">
+        function tableToCSV() {
+            // Variable to store the final csv data
+            let csv_data = [];
+            // Get each row data
+            let rows = document.getElementsByTagName('tr');
+            for (let i = 0; i < rows.length; i++) {
+                // Get each column data
+                let cols = rows[i].querySelectorAll('td,th');
+                // Stores each csv row data
+                let csvrow = [];
+                for (let j = 0; j < cols.length; j++) {
+                    // Get the text data of each cell
+                    // of a row and push it to csvrow
+                    csvrow.push(cols[j].innerHTML);
+                }
+                // Combine each column value with comma
+                csv_data.push(csvrow.join(","));
+            }
+            // Combine each row data with new line character
+            csv_data = csv_data.join('\n');
+            // Call this function to download csv file  
+            downloadCSVFile(csv_data);
+        }
+        function downloadCSVFile(csv_data) {
+            // Create CSV file object and feed
+            // our csv_data into it
+            CSVFile = new Blob([csv_data], {
+                type: "text/csv"
+            });
+            // Create to temporary link to initiate
+            // download process
+            let temp_link = document.createElement('a');
+            // Download csv file
+            temp_link.download = "PingMakertarget.csv";
+            let url = window.URL.createObjectURL(CSVFile);
+            temp_link.href = url;
+            // This link should not be displayed
+            temp_link.style.display = "none";
+            document.body.appendChild(temp_link);
+            // Automatically click the link to
+            // trigger download
+            temp_link.click();
+            document.body.removeChild(temp_link);
+        }
+    </script>
+  
 <?php
 
 // Include Composer autoload (make sure it's included to load the MongoDB library)
@@ -33,8 +84,8 @@ $result = $collection->find($Query);
 if (!empty($Target)){
 //Function
     //set up table
+    echo "<h1>$Target</h1><br>";  
     echo "<div style='height:500px; width:600px; overflow: auto;'><br>";
-    echo "<h1>$Target</h1><br>";
     echo "<table>";
     echo "<tr>";
     echo "<th>Time Of Ping</th>";
