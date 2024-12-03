9f89a25e7b870e2b45567a993169f45d9bb5b998
Dominic Kavalary <128938538+DominicKavalary@users.noreply.github.com>
2024-12-03 17:50:42 -0600

    Update status.php

--- status.php
+++ status.php
@@ -16,17 +16,37 @@
 // Include Composer autoload (make sure it's included to load the MongoDB library)
 require 'vendor/autoload.php'; // Path to Composer's autoload file
 
-//MongoDB Test
+//MongoDB PHP Connection Test
 echo "<h1>MongoDB Connection</h1>";
 try {
 	// Create a new MongoDB client to connect to the MongoDB server
   $client = new MongoDB\Client("mongodb://localhost:27017"); // Change if your MongoDB is hosted elsewhere
+  echo "Connection to database using PHP succesfull";
 } catch (Exception $ex) {
-	echo "Could not make connection to database";
+  echo "Could not make connection to database with PHP";
   echo PHP_EOL;
 }
 
-//Python Script Test
+//Mongodb Service Test
+echo "<h1>Mongodb Service</h1>";
+try {
+	// Create a new MongoDB client to connect to the MongoDB server
+  $output=null;
+  $retval=null;
+  exec('systemctl status mongod', $output, $retval);
+  echo "Returned with status $retval and output:\n";
+  foreach ($output as $line) {
+    echo "<br>$line";
+    if (str_contains($line,'CGroup',)) {
+      break;
+    }
+  }
+} catch (Exception $ex) {
+	echo "Error running systemctl status mongod using PHP";
+  echo PHP_EOL;
+}
+
+//Python Script Service Test
 echo "<h1>PingMaker Service</h1>";
 try {
 	// Create a new MongoDB client to connect to the MongoDB server
@@ -34,9 +54,14 @@ try {
   $retval=null;
   exec('systemctl status PingMaker', $output, $retval);
   echo "Returned with status $retval and output:\n";
-  print_r($output);
+  foreach ($output as $line) {
+    echo "<br>$line";
+    if (str_contains($line,'CGroup',)) {
+      break;
+    }
+  }
 } catch (Exception $ex) {
-	echo "PingMaker Service not running";
+	echo "Error running systemctl status PingMaker using PHP";
   echo PHP_EOL;
 }
 
