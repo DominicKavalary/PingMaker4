5647acaa7ecd86a1b3669d797c3c453dbfc55b17
Dominic Kavalary <128938538+DominicKavalary@users.noreply.github.com>
2024-12-04 14:27:24 -0600

    Update addtargets.php

--- addtargets.php
+++ addtargets.php
@@ -60,22 +60,7 @@ if (!empty($Target)){
 // Get list of current targets and print
 $result = $collection->find();
 //set up table
-echo "<h1>Target List</h1>", PHP_EOL;  
-echo "<div style='height:500px; width:600px; overflow: auto;'>", PHP_EOL;
-echo "<table>", PHP_EOL;
-echo "<tr><th>Target</th><th>Description</th><th>Delay</th></tr>";
-    //insert rows into table
-foreach ($result as $entry) {
-    echo "<tr><td>";
-    echo json_encode($entry['Target']);
-    echo "</td><td>";
-    echo json_encode($entry['Description']);
-    echo "</td><td>";
-    echo json_encode($entry['Delay']);
-    echo "</td></tr>";
-    echo PHP_EOL;
-}
-echo "</table></div>";
+GetTargetTable($result);
 
 ?>
 </body>
