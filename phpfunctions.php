99bba94625b5b76f5497738cbdd56a4eca01132a
Dominic Kavalary <128938538+DominicKavalary@users.noreply.github.com>
2024-12-04 15:07:52 -0600

    Update phpfunctions.php

--- phpfunctions.php
+++ phpfunctions.php
@@ -17,4 +17,25 @@ function GetTargetTable($result){
   }
   echo "</table></div>";
 }
+function GetRequestTable($result){
+  echo "<h1>$Target</h1>", PHP_EOL;  
+  echo "<div style='height:500px; width:600px; overflow: auto;'>", PHP_EOL;
+  echo "<table>", PHP_EOL;
+  echo "<tr><th>Time Of Ping</th><th>Packet Loss</th><th>Response Time</th><th>Response Time</th>Error Note</tr>";
+  //insert rows into table
+  foreach ($result as $entry) {
+    echo "<tr><td>";
+    echo json_encode($entry['timeOfPing']);
+    echo "</td><td>";
+    echo json_encode($entry['packetLoss']);
+    echo "</td><td>";
+    echo json_encode($entry['responseTime']);
+    echo "</td><td>";
+    echo json_encode($entry['errorNote']);
+    echo "</td></tr>";
+    echo PHP_EOL;
+  }
+  echo "</table></div>";
+}
+
 ?>
