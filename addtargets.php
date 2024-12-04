852d3459430948d7eefedccbb030208c8e33886e
Dominic Kavalary <128938538+DominicKavalary@users.noreply.github.com>
2024-12-04 14:52:37 -0600

    Update addtargets.php

--- addtargets.php
+++ addtargets.php
@@ -6,26 +6,26 @@
     <ul>
       <li><a href="index.html">Home</a></li>
       <li><a href="request.html">Requests</a></li>
-      <li><a href="targets.html">Targets</a></li>
+      <li><a href="targets.php">Targets</a></li>
       <li><a href="status.php">Status</a></li>
     </ul>
   </head>
 <body>
     <h1>Database Functions</h1>
     <h2>Add Target</h2>
-    <form id="runRequest" action="addtargets.php" method="POST">
+    <form id="runRequest" action="targets.php" method="POST">
     <input type="text" name="target" placeholder="IP or Hostname" required><br>
     <input type="text" name="description" placeholder="Description" required><br>
     <input type="text" name="delay" placeholder="Ping Delay In Seconds" required><br>
     <input type="submit" value="Add" name="submit">
     </form>
     <h2>Removing Target</h2>
-    <form id="runRequest" action="addtargets.php" method="POST">
+    <form id="runRequest" action="targets.php" method="POST">
     <input type="text" name="target" placeholder="IP or Hostname" required><br>
     <input type="submit" value="Remove" name="submit">
     </form>
     <h2>Update Target</h2>
-    <form id="runRequest" action="addtargets.php" method="POST">
+    <form id="runRequest" action="targets.php" method="POST">
     <input type="text" name="target" placeholder="IP or Hostname" required><br>
     <input type="text" name="description" placeholder="Description" required><br>
     <input type="text" name="delay" placeholder="Ping Delay In Seconds" required><br>
