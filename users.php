52dc4ed49a837eafd4dee153455108a76dcd2c72
Dominic Kavalary <128938538+DominicKavalary@users.noreply.github.com>
2024-12-18 00:13:27 -0600

    Update users.php
    guest cant see user page

--- users.php
+++ users.php
@@ -24,7 +24,53 @@ if (!isset($_SESSION['loggedin'])) {
 	
 <?php
 if ($_SESSION['role'] != "Admin") {
-	echo "<h1 style='color:red;'>You need admin permissions in order to edit User database. Forms will do nothing until current user is changed</h1><br>";
+	echo "<h1 style='color:red;'>You need admin permissions in order to edit User database</h1><br>";
+	exit;
+} else{
+	// Include Composer autoload (make sure it's included to load the MongoDB library)
+	require 'vendor/autoload.php'; // Path to Composer's autoload file
+	require 'phpfunctions.php';
+	// Create a new MongoDB client to connect to the MongoDB server
+	$client = new MongoDB\Client("mongodb://localhost:27017"); // Change if your MongoDB is hosted elsewhere
+	$database = $client->database;
+	$collection = $database->users;
+	
+	// Get variables, some may have Target some may have description, remove only has target
+	$Username = $_POST["username"];
+	$Password = $_POST["password"];
+	$Role = $_POST["role"];
+	$Submit = $_POST["submit"];
+	
+	//Check if the target is null, if it is, dont do anything
+	if (!empty($Username)){
+	//Function to find if target is already in database, if it isnt, add it with the description
+	    if ($Submit == "Add"){
+	      $result = $collection->findOne(['Username' => $Username]);
+	      if ($result['Username'] == $Username){
+	          echo "<h1 style='color:red;'>Error: User already in database</h1><br>";
+	      }else {
+	          $collection->insertOne(['Username' => $Username, 'Password' => hash('sha256',$Password), 'Role' => $Role,]);
+	          echo "<h1 style='color:green;'>User added to database</h1><br>";
+	      }
+	    } else if ($Submit == "Remove"){
+	      $result = $collection->findOne(['Username' => $Username]);
+	      if ($result['Username'] == $Username){
+	          $collection->deleteOne(['Username' => $Username,]);
+	          echo "<h1 style='color:green;'>User removed from database</h1><br>";
+	      }else {
+	          echo "<h1 style='color:red;'>Error: User not found in database</h1><br>";
+	      }
+	    } else if ($Submit == "Update"){
+	      $result = $collection->findOne(['Username' => $Username]);
+	      if ($result['Username'] == $Username){
+	          $collection->updateOne([ 'Username' => $Username ], [ '$set' => [ 'Password' => $Password ]]);
+	          $collection->updateOne([ 'Username' => $Username ], [ '$set' => [ 'Role' => $Role ]]);
+	          echo "<h1 style='color:green;'>User updated</h1><br>";
+	      }else {
+	          echo "<h1 style='color:red;'>Error: User not found in database</h1><br>";
+	      }
+	    }
+	}
 }
 ?>
     <h1>Database Functions</h1>
@@ -58,52 +104,6 @@ if ($_SESSION['role'] != "Admin") {
     <input type="submit" value="Update" name="submit">
     </form>
 <?php
-
-// Include Composer autoload (make sure it's included to load the MongoDB library)
-require 'vendor/autoload.php'; // Path to Composer's autoload file
-require 'phpfunctions.php';
-// Create a new MongoDB client to connect to the MongoDB server
-$client = new MongoDB\Client("mongodb://localhost:27017"); // Change if your MongoDB is hosted elsewhere
-$database = $client->database;
-$collection = $database->users;
-
-// Get variables, some may have Target some may have description, remove only has target
-$Username = $_POST["username"];
-$Password = $_POST["password"];
-$Role = $_POST["role"];
-$Submit = $_POST["submit"];
-
-//Check if the target is null, if it is, dont do anything
-if (!empty($Username) && $_SESSION['role'] == "Admin"){
-//Function to find if target is already in database, if it isnt, add it with the description
-    if ($Submit == "Add"){
-      $result = $collection->findOne(['Username' => $Username]);
-      if ($result['Username'] == $Username){
-          echo "<h1 style='color:red;'>Error: User already in database</h1><br>";
-      }else {
-          $collection->insertOne(['Username' => $Username, 'Password' => hash('sha256',$Password), 'Role' => $Role,]);
-          echo "<h1 style='color:green;'>User added to database</h1><br>";
-      }
-    } else if ($Submit == "Remove"){
-      $result = $collection->findOne(['Username' => $Username]);
-      if ($result['Username'] == $Username){
-          $collection->deleteOne(['Username' => $Username,]);
-          echo "<h1 style='color:green;'>User removed from database</h1><br>";
-      }else {
-          echo "<h1 style='color:red;'>Error: User not found in database</h1><br>";
-      }
-    } else if ($Submit == "Update"){
-      $result = $collection->findOne(['Username' => $Username]);
-      if ($result['Username'] == $Username){
-          $collection->updateOne([ 'Username' => $Username ], [ '$set' => [ 'Password' => $Password ]]);
-          $collection->updateOne([ 'Username' => $Username ], [ '$set' => [ 'Role' => $Role ]]);
-          echo "<h1 style='color:green;'>User updated</h1><br>";
-      }else {
-          echo "<h1 style='color:red;'>Error: User not found in database</h1><br>";
-      }
-    }
-}
-
 // Get list of current targets and print
 $result = $collection->find();
 //set up table
