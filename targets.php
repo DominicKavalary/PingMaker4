fa4ba72cc4fe40e999a89190f1ec118531ecb4f1
Dominic Kavalary <128938538+DominicKavalary@users.noreply.github.com>
2024-12-18 00:08:40 -0600

    Update targets.php
    guest user unable to see target forms

--- targets.php
+++ targets.php
@@ -24,7 +24,53 @@ if (!isset($_SESSION['loggedin'])) {
 	
 <?php
 if ($_SESSION['role'] != "Admin") {
-	echo "<h1 style='color:red;'>You need admin permissions in order to edit target database. Forms will do nothing until current user is changed</h1><br>";
+	echo "<h1 style='color:red;'>You need admin permissions in order to edit target database</h1><br>";
+	exit;
+} else {
+	// Include Composer autoload (make sure it's included to load the MongoDB library)
+	require 'vendor/autoload.php'; // Path to Composer's autoload file
+	require 'phpfunctions.php';
+	// Create a new MongoDB client to connect to the MongoDB server
+	$client = new MongoDB\Client("mongodb://localhost:27017"); // Change if your MongoDB is hosted elsewhere
+	$database = $client->database;
+	$collection = $database->targets;
+	
+	// Get variables, some may have Target some may have description, remove only has target
+	$Target = $_POST["target"];
+	$Description = $_POST["description"];
+	$Delay = $_POST["delay"];
+	$Submit = $_POST["submit"];
+	
+	//Check if the target is null, if it is, dont do anything
+	if (!empty($Target)){
+	//Function to find if target is already in database, if it isnt, add it with the description
+	    if ($Submit == "Add"){
+	      $result = $collection->findOne(['Target' => $Target]);
+	      if ($result['Target'] == $Target){
+	          echo "<h1 style='color:red;'>Error: Target already in database</h1><br>";
+	      }else {
+	          $collection->insertOne(['Target' => $Target, 'Description' => $Description, 'Delay' => $Delay,]);
+	          echo "<h1 style='color:green;'>Target added to database</h1><br>";
+	      }
+	    } else if ($Submit == "Remove"){
+	      $result = $collection->findOne(['Target' => $Target]);
+	      if ($result['Target'] == $Target){
+	          $collection->deleteOne(['Target' => $Target,]);
+	          echo "<h1 style='color:green;'>Target removed from database</h1><br>";
+	      }else {
+	          echo "<h1 style='color:red;'>Error: Target not found in database</h1><br>";
+	      }
+	    } else if ($Submit == "Update"){
+	      $result = $collection->findOne(['Target' => $Target]);
+	      if ($result['Target'] == $Target){
+	          $collection->updateOne([ 'Target' => $Target ], [ '$set' => [ 'Description' => $Description ]]);
+	          $collection->updateOne([ 'Target' => $Target ], [ '$set' => [ 'Delay' => $Delay ]]);
+	          echo "<h1 style='color:green;'>Target updated</h1><br>";
+	      }else {
+	          echo "<h1 style='color:red;'>Error: Target not found in database</h1><br>";
+	      }
+	    }
+	}
 }
 ?>
     <h1>Database Functions</h1>
@@ -39,10 +85,6 @@ if ($_SESSION['role'] != "Admin") {
     <form id="runRequest" action="targets.php" method="POST">
     <select name="target" placeholder="IP or Hostname">
     <?php
-        require 'vendor/autoload.php';
-	$client = new MongoDB\Client("mongodb://localhost:27017"); // Change if your MongoDB is hosted elsewhere
-	$database = $client->database;
-	$collection = $database->targets;
 	$result = $collection->find();
 	foreach ($result as $entry) {
 		$Value = $entry['Target'];
@@ -57,10 +99,6 @@ if ($_SESSION['role'] != "Admin") {
     <form id="runRequest" action="targets.php" method="POST">
     <select name="target" placeholder="IP or Hostname">
     <?php
-        require 'vendor/autoload.php';
-	$client = new MongoDB\Client("mongodb://localhost:27017"); // Change if your MongoDB is hosted elsewhere
-	$database = $client->database;
-	$collection = $database->targets;
 	$result = $collection->find();
 	foreach ($result as $entry) {
 		$Value = $entry['Target'];
@@ -73,51 +111,6 @@ if ($_SESSION['role'] != "Admin") {
     <input type="submit" value="Update" name="submit">
     </form>
 <?php
-
-// Include Composer autoload (make sure it's included to load the MongoDB library)
-require 'vendor/autoload.php'; // Path to Composer's autoload file
-require 'phpfunctions.php';
-// Create a new MongoDB client to connect to the MongoDB server
-$client = new MongoDB\Client("mongodb://localhost:27017"); // Change if your MongoDB is hosted elsewhere
-$database = $client->database;
-$collection = $database->targets;
-
-// Get variables, some may have Target some may have description, remove only has target
-$Target = $_POST["target"];
-$Description = $_POST["description"];
-$Delay = $_POST["delay"];
-$Submit = $_POST["submit"];
-
-//Check if the target is null, if it is, dont do anything
-if (!empty($Target) && $_SESSION['role'] == "Admin"){
-//Function to find if target is already in database, if it isnt, add it with the description
-    if ($Submit == "Add"){
-      $result = $collection->findOne(['Target' => $Target]);
-      if ($result['Target'] == $Target){
-          echo "<h1 style='color:red;'>Error: Target already in database</h1><br>";
-      }else {
-          $collection->insertOne(['Target' => $Target, 'Description' => $Description, 'Delay' => $Delay,]);
-          echo "<h1 style='color:green;'>Target added to database</h1><br>";
-      }
-    } else if ($Submit == "Remove"){
-      $result = $collection->findOne(['Target' => $Target]);
-      if ($result['Target'] == $Target){
-          $collection->deleteOne(['Target' => $Target,]);
-          echo "<h1 style='color:green;'>Target removed from database</h1><br>";
-      }else {
-          echo "<h1 style='color:red;'>Error: Target not found in database</h1><br>";
-      }
-    } else if ($Submit == "Update"){
-      $result = $collection->findOne(['Target' => $Target]);
-      if ($result['Target'] == $Target){
-          $collection->updateOne([ 'Target' => $Target ], [ '$set' => [ 'Description' => $Description ]]);
-          $collection->updateOne([ 'Target' => $Target ], [ '$set' => [ 'Delay' => $Delay ]]);
-          echo "<h1 style='color:green;'>Target updated</h1><br>";
-      }else {
-          echo "<h1 style='color:red;'>Error: Target not found in database</h1><br>";
-      }
-    }
-}
 
 // Get list of current targets and print
 $result = $collection->find();
