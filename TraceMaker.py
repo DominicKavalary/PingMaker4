9dc0784b1a554897719bd7ecfe6151a2c051450d
Dominic Kavalary <128938538+DominicKavalary@users.noreply.github.com>
2025-03-18 15:58:01 -0500

    Update TraceMaker.py

--- TraceMaker.py
+++ TraceMaker.py
@@ -2,10 +2,37 @@
 sudo apt install traceroute -y
 make a traceroutes collection and a tracemaps collection in mongodb
 
-
+# Create the TraceObject object. this will be the core of our TraceTree
+class TraceObject:
+  #  define the object, it has two variables, address and nexthops. nexthops will be an array that holds traceobjects. address will be the hop address
+  def __init__(self, Address):
+    self.address = Address
+    self.nexthops = []
+    # if the address inputed into the object matches a next hop it has, return true
+  def ifHopMatches(self, Address):
+    if self.nexthops:
+      for hop in self.nexthops:
+        if hop.address == Address:
+          return True
+      return False
+    else:
+      return False
+      # add a traceobject to the array of next hops
+  def addHop(self, Hop):
+    self.nexthops.append(Hop)
 
 #TraceMaker frunction to get the route to your targets from the server
 def TraceMaker(Target, Delay):
+  # check if the tree has started being built. if not, keep the flag false to start to build it, if it has, dont keep checking
+  TreeBuilt = False
+  client = pymongo.MongoClient(host="localhost", port=27017)
+  db = client["database"]
+  collection = db["tracemaps"]
+  if collection.find_one({'Target': Target}):
+    TreeBuilt = True
+  client.close()
+
+  # Set up the loop to keep doing traceroutes
   while 1==1:
   # setting up array for hops to go in
     HopArray = []
@@ -36,8 +63,20 @@ def TraceMaker(Target, Delay):
     # insert data
     collection.insert_one(data)
 
+    # Building the tree
 
+   def buildTree(HopArray):
+     # Hop array depth tracker
+     HopTracker = 0
+     # the first node in the tree is the machine itself
+     Node=TraceObject("Self")
+     #Now you need to check to see if the next node exists and matches the next recorded hop
+     Node.
 
+    if TreeBuilt == False:
+      # build innitial tree
+      for item in HopArray:
+        TreeNode = TraceObject(item)
 
 
 
