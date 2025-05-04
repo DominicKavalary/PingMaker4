11950350253b23a95aa25b3cf9ea981ae0f8c389
Dominic Kavalary <128938538+DominicKavalary@users.noreply.github.com>
2025-05-04 15:50:05 -0500

    Update PingSetup.py

--- PingSetup.py
+++ PingSetup.py
@@ -1,7 +1,5 @@
 #### Setup ####
 ###Imports###
-import pymongo
-import hashlib
 import subprocess
 
 
@@ -22,7 +20,7 @@ print("------ Installing PHP and Pecl Installing Mongodb ------")
 subprocess.run(['apt install -y php-dev'], shell=True)
 subprocess.run(['pecl install -y mongodb'], shell=True)
 print("Adding mongodb.so extensions to php file")
-with open("example.txt", "a") as file:
+with open("/etc/php/8.3/cli/php.ini", "a") as file:
     file.write("\nextension=mongodb.so")
 print("Installing libapache-php")
 subprocess.run(['apt install libapache2-mod-php8.3'], shell=True)
@@ -43,6 +41,9 @@ subprocess.run(['wget https://raw.githubusercontent.com/DominicKavalary/PingMake
 subprocess.run(['wget https://raw.githubusercontent.com/DominicKavalary/PingMaker4/refs/heads/main/PingMaker.service -P /etc/systemd/system/'], shell=True)
 print("Installing pymongo and traceroute")
 subprocess.run(['apt install -y python3-pymongo'], shell=True)
+print("------ Downloading and running database innitialization script ------")
+subprocess.run(['wget https://raw.githubusercontent.com/DominicKavalary/PingMaker4/refs/heads/main/PingSetup2.py'], shell=True)
+subprocess.run(['python3 PingSetup2.py'], shell=True)
 subprocess.run(['apt install -y traceroute'], shell=True)
 print("Enbaling service and reloading daemon")
 subprocess.run(['systemctl enable PingMaker.service'], shell=True)
@@ -64,31 +65,13 @@ subprocess.run(['wget https://raw.githubusercontent.com/DominicKavalary/PingMake
 subprocess.run(['wget https://raw.githubusercontent.com/DominicKavalary/PingMaker4/refs/heads/main/request.php'], shell=True)
 subprocess.run(['wget https://raw.githubusercontent.com/DominicKavalary/PingMaker4/refs/heads/main/targets.php'], shell=True)
 subprocess.run(['wget https://raw.githubusercontent.com/DominicKavalary/PingMaker4/refs/heads/main/status.php'], shell=True)
-subprocess.run(['https://raw.githubusercontent.com/DominicKavalary/PingMaker4/refs/heads/main/home.php'], shell=True)
-subprocess.run(['https://raw.githubusercontent.com/DominicKavalary/PingMaker4/refs/heads/main/login.php'], shell=True)
-subprocess.run(['https://raw.githubusercontent.com/DominicKavalary/PingMaker4/refs/heads/main/phpfunctions.php'], shell=True)
-subprocess.run(['https://raw.githubusercontent.com/DominicKavalary/PingMaker4/refs/heads/main/logout.php'], shell=True)
+subprocess.run(['wget https://raw.githubusercontent.com/DominicKavalary/PingMaker4/refs/heads/main/home.php'], shell=True)
+subprocess.run(['wget https://raw.githubusercontent.com/DominicKavalary/PingMaker4/refs/heads/main/login.php'], shell=True)
+subprocess.run(['wget https://raw.githubusercontent.com/DominicKavalary/PingMaker4/refs/heads/main/phpfunctions.php'], shell=True)
+subprocess.run(['wget https://raw.githubusercontent.com/DominicKavalary/PingMaker4/refs/heads/main/logout.php'], shell=True)
 print("Composer Require step")
 subprocess.run(['composer require mongodb/mongodb'], shell=True)
 
-
-####### Innitializing Database and Adding Default Admin User #######
-print("------ Creating default admin user and innitializing database tables ------")
-Username = "admin"
-Password = "admin"
-Password = hashlib.sha256(Password.encode('utf-8')).hexdigest()
-AdminUser = { "Username": Username, "Password": Password, "Role": "Admin" }
-client = pymongo.MongoClient(host="localhost", port=27017)
-db = client["database"]
-collection = db["targets"]
-collection = db["errors"]
-collection.create_index([("createdAt", 1)], expireAfterSeconds=604800)
-collection = db["collection"]
-collection.create_index([("createdAt", 1)], expireAfterSeconds=604800)
-collection = db["users"]
-collection.insert_one(AdminUser)
-collection = db["traceroutes"]
-collection.create_index([("createdAt", 1)], expireAfterSeconds=604800)
-collection = db["tracemaps"]
-client.close()
+####### Done #######
 print("------ Innitial Setup Complete ------")
+
