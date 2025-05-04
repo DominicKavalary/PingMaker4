c2534261fa55e4ab336aec3f94065a192a103f7c
Dominic Kavalary <128938538+DominicKavalary@users.noreply.github.com>
2025-05-04 15:48:22 -0500

    Update PingSetup2.py

--- PingSetup2.py
+++ PingSetup2.py
@@ -5,10 +5,12 @@ import subprocess
 
 ####### Innitializing Database and Adding Default Admin User #######
 print("------ Creating default admin user and innitializing database tables ------")
+print("Making Default User")
 Username = "admin"
 Password = "admin"
 Password = hashlib.sha256(Password.encode('utf-8')).hexdigest()
 AdminUser = { "Username": Username, "Password": Password, "Role": "Admin" }
+print("Connecting to mongodb and innitializing tables and admin user")
 client = pymongo.MongoClient(host="localhost", port=27017)
 db = client["database"]
 collection = db["targets"]
