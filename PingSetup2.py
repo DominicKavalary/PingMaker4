777952d72160971a4eaa466b6fbcd78ee94962b4
Dominic Kavalary <128938538+DominicKavalary@users.noreply.github.com>
2025-05-04 15:47:10 -0500

    Create PingSetup2.py

---
+++ PingSetup2.py
@@ -0,0 +1,24 @@
+###Imports###
+import pymongo
+import hashlib
+import subprocess
+
+####### Innitializing Database and Adding Default Admin User #######
+print("------ Creating default admin user and innitializing database tables ------")
+Username = "admin"
+Password = "admin"
+Password = hashlib.sha256(Password.encode('utf-8')).hexdigest()
+AdminUser = { "Username": Username, "Password": Password, "Role": "Admin" }
+client = pymongo.MongoClient(host="localhost", port=27017)
+db = client["database"]
+collection = db["targets"]
+collection = db["errors"]
+collection.create_index([("createdAt", 1)], expireAfterSeconds=604800)
+collection = db["collection"]
+collection.create_index([("createdAt", 1)], expireAfterSeconds=604800)
+collection = db["users"]
+collection.insert_one(AdminUser)
+collection = db["traceroutes"]
+collection.create_index([("createdAt", 1)], expireAfterSeconds=604800)
+collection = db["tracemaps"]
+client.close()
