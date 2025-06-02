5bb0cb63d095fc32b266ef00a9ab8e22a6ff610a
Dominic Kavalary <128938538+DominicKavalary@users.noreply.github.com>
2025-06-02 15:14:43 -0500

    Create PingSetup3.py

---
+++ PingSetup3.py
@@ -0,0 +1,30 @@
+###Imports###
+import subprocess
+
+####### SSL Setup #######
+hostname = "pingmaker.local"
+subprocess.run(['a2enmod ssl'], shell=True)
+subprocess.run(['systemctl restart apache2'], shell=True)
+subprocess.run([f'openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt -subj "/C=US/ST=None/L=None/O=None/OU=None/CN={hostname}"'], shell=True)
+subprocess.run(['touch /etc/apache2/sites-available/ssl-selfsigned.conf'], shell=True)
+ssl_conf = f"""
+<VirtualHost *:443>
+   ServerName {hostname}
+   DocumentRoot /var/www/html
+
+   SSLEngine on
+   SSLCertificateFile /etc/ssl/certs/apache-selfsigned.crt
+   SSLCertificateKeyFile /etc/ssl/private/apache-selfsigned.key
+</VirtualHost>
+<VirtualHost *:80>
+	ServerName {hostname}
+	Redirect / https://{hostname}/
+</VirtualHost>
+"""
+with open("/etc/apache2/sites-available/ssl-selfsigned.conf", "a") as file:
+    file.write(ssl_conf)
+subprocess.run(['a2ensite ssl-selfsigned.conf'], shell=True)
+subprocess.run(['shutdown -r now'], shell=True)
+
+
+
